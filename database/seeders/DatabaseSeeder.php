06d79e90317643822f56b420144ebf5bd41bbf23
rey <alasasrey5@gmail.com>
2025-02-25 14:28:20 +0800

    Activity 2: Add new user.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,27 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        User::factory(1000)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Students::create([
+            'name' => 'Male 1',
+            'age' => 25,
+            'gender' => 'male'
+        ]);
+        Students::create([
+            'name' => 'Female 1',
+            'age' => 27,
+            'gender' => 'female'
+        ]);
+        Students::create([
+            'name' => 'Female 2',
+            'age' => 23,
+            'gender' => 'female'
         ]);
     }
 }
